547281e70e7416d4fe7b8f3a160e775a135e2646
psuman10 <psuman9840@gmail.com>
2022-01-07 22:21:57 +0545

    working on admin sidebar

--- admin/src/dashboard/Sidebar.js
+++ admin/src/dashboard/Sidebar.js
@@ -4,10 +4,14 @@ import {Link} from 'react-router-dom'
 const Sidebar = () => {
     return (
         
-         <div className="col-md-3 col-lg-2" id="sidebar" style={{backgroundColor:"#241e46"}}>
+         <div className="col-md-3 col-lg-2 sidebar-offcanvas pl-0" id="sidebar" role="navigation" style={{backgroundColor:"#241e46"}}>
           
             <ul className="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
                 <li className="nav-item mb-2 mt-3"><Link className="nav-link" to="/" ><h5 style={{fontSize:"25px",color:"orangered"}}>Your's Choice</h5></Link></li>
+                <li className="nav-item mb-2 "><Link className="nav-link text-white" to="/"><i className="fas fa-user font-weight-bold"></i> <span className="ml-3">Dashboard</span></Link></li>
+                <li className="nav-item mb-2"><Link className="nav-link text-white" to="/"><i className="fas fa-users font-weight-bold"></i> <span className="ml-3">Users</span></Link></li>
+                <li className="nav-item mb-2"><Link className="nav-link text-white" to="/"><i className="fa fa-retweet font-weight-bold"></i><span className="ml-3">Bookings</span></Link></li>
+                
             </ul>
        </div>
     )
