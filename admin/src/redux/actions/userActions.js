ee885abadea993f6157a1e2904373f1c0f31ec91
psuman10 <psuman9840@gmail.com>
2022-01-11 11:14:26 +0545

    working on admin fetching users data from backend

---
+++ admin/src/redux/actions/userActions.js
@@ -0,0 +1,13 @@
+export const getAllUsers=()=>async dispatch=>{
+
+    dispatch({type: 'LOADING' , payload:true})
+
+    try {
+        const response = await axios.get('/api/users/getallusers')
+        dispatch({type: 'GET_ALL_USERS', payload:response.data})
+        dispatch({type: 'LOADING' , payload:false})
+    } catch (error) {
+        console.log(error)
+        dispatch({type: 'LOADING' , payload:false})
+    }
+}
