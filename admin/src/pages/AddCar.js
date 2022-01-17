0057e09c7b78efedea3ff451215d6467b480e8c7
psuman10 <psuman9840@gmail.com>
2022-01-17 21:00:01 +0545

    added add car

---
+++ admin/src/pages/AddCar.js
@@ -0,0 +1,54 @@
+import { Col , Row , Form , Input} from 'antd'
+import React from 'react'
+import { useDispatch} from 'react-redux'
+import { addCar } from '../redux/actions/carsActions'
+
+
+function AddCar() {
+
+    const dispatch = useDispatch()
+
+    function onFinish(values){
+
+         values.bookedTimeSlots=[]
+
+         dispatch(addCar(values))
+        
+    }
+
+    return (
+       
+               <Row justify='center ' style={{marginTop:"80px"}}>
+                   <Col lg={12} sm={24} xs={24} className='p-2'>
+                       <Form className='bs1 p-2' layout='vertical' onFinish={onFinish}>
+                           <h3>Add New Car</h3>
+                           <hr />
+                           <Form.Item name='name' label='Car name' rules={[{required: true}]}>
+                               <Input/>
+                           </Form.Item>
+                           <Form.Item name='image' label='Image url' rules={[{required: true}]}>
+                               <Input/>
+                           </Form.Item>
+                           <Form.Item name='rentPerHour' label='Rent per hour' rules={[{required: true}]}>
+                               <Input/>
+                           </Form.Item>
+                           <Form.Item name='capacity' label='Capacity' rules={[{required: true}]}>
+                               <Input/>
+                           </Form.Item>
+                           <Form.Item name='fuelType' label='Fuel Type' rules={[{required: true}]}>
+                               <Input/>
+                           </Form.Item>
+
+                           <div className='text-right'>
+                           <button className='btn1'>ADD CAR</button>
+                           </div>
+
+                       </Form>
+                   </Col>
+               </Row>
+
+            
+    )
+}
+
+export default AddCar
