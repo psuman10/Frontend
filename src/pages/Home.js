93ebad3947e15c2394b53421cce5c2dfde3af5be
psuman10 <psuman9840@gmail.com>
2022-01-24 17:37:41 +0545

    made a date picker to check the booking details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,10 @@ import Slider from '../slider/slider'
 import {getAllCars} from '../redux/actions/carsActions'
 import { useSelector , useDispatch } from 'react-redux'
 import React , {useEffect, useState} from 'react'
-import { Col, Row} from 'antd'
+import { Col, Row, DatePicker} from 'antd'
 import {Link} from 'react-router-dom'
+import moment from 'moment'
+const {RangePicker} = DatePicker
 function Home(){
 
     const {cars} = useSelector(state=>state.carsReducer)
@@ -21,11 +23,64 @@ function Home(){
         
     }, [cars])
     
+    function setFilter(values){
+
+        var selectedFrom = moment(values[0] , 'MMM DD yyyy HH:mm')
+        var selectedTo = moment(values[1] , 'MMM DD yyyy HH:mm')
+
+        var temp=[]
+
+        for(var car of cars){
+
+              if(car.bookedTimeSlots.length == 0){
+                  temp.push(car)
+              }
+              else{
+
+                   for(var booking of car.bookedTimeSlots) {
+
+                       if(selectedFrom.isBetween(booking.from , booking.to) ||
+                       selectedTo.isBetween(booking.from , booking.to) || 
+                       moment(booking.from).isBetween(selectedFrom , selectedTo) ||
+                       moment(booking.to).isBetween(selectedFrom , selectedTo)
+                       )
+                       {
+
+                       }
+                       else{
+                        temp.push(car)
+                    }
+
+                }
+
+            }
+        }
+
+        setTotalcars(temp)
+ 
+
+    }
 
     
     return(
         <DefaultLayout >
             <Slider></Slider>
+
+            <Row className='mt-3' justify='center'>
+                 
+                 <Col lg={20} sm={24} className='d-flex justify-content-center date_picker'>
+
+                     <RangePicker showTime={{format: 'HH:mm'}} format='MMM DD yyyy HH:mm' onChange={setFilter}/>
+                 
+                 </Col>
+
+             </Row>
+
+
+
+
+
+
             <Row justify='center' gutter={16}>
 
                    {totalCars.map(car=>{
